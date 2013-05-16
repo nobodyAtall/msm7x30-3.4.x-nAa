79307d6b0120e6de73e9cf0d2f09fef83b0abe7e
nobodyAtall <tsogkas@ceid.upatras.gr>
2013-05-16 20:17:02 +0300

    Revert "wl12xx: hardcode wl12xx_git_head"
    This reverts commit b7d4e07ae241800500fedabc9b3bab45941f014e.

--- net/compat-wireless/drivers/net/wireless/wl12xx/version.h
+++ net/compat-wireless/drivers/net/wireless/wl12xx/version.h
@@ -1,2 +1,2 @@
 static const char *wl12xx_timestamp = __TIMESTAMP__;
-static const char *wl12xx_git_head = "ol_R5.SP4.01-2-g1aa2c8e";
+static const char *wl12xx_git_head = "M7630AABBQMLZA40901040-224-g7d4cc4f-dirty";
