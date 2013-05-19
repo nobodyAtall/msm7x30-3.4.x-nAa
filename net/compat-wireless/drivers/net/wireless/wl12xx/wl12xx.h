247adc29c34bf215933073ac2c09fd953d0c004b
Peter Wagner <puchu@gmx.at>
2013-05-19 13:19:56 +0200

    fix wifi firmware location

--- net/compat-wireless/drivers/net/wireless/wl12xx/wl12xx.h
+++ net/compat-wireless/drivers/net/wireless/wl12xx/wl12xx.h
@@ -39,7 +39,7 @@
 #include "ini.h"
 
 #define WL127X_FW_NAME_MULTI	"ti-connectivity/wl127x-fw-4-mr.bin"
-#define WL127X_FW_NAME_SINGLE	"../../../firmware/wl127x-fw-4-sr.bin"
+#define WL127X_FW_NAME_SINGLE	"ti-connectivity/wl127x-fw-4-sr.bin"
 #define WL127X_PLT_FW_NAME	"ti-connectivity/wl127x-fw-4-plt.bin"
 
 #define WL128X_FW_NAME_MULTI	"ti-connectivity/wl128x-fw-4-mr.bin"
