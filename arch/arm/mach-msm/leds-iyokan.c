d7370ebd989dd91d9bd4ad1f7e7e11acdf5e606e
nobodyAtall <tsogkas@ceid.upatras.gr>
2013-05-19 23:36:02 +0300

    mogami: adjust to new as3676 platform driver - not complete for some devices

--- arch/arm/mach-msm/leds-iyokan.c
+++ arch/arm/mach-msm/leds-iyokan.c
@@ -1,6 +1,6 @@
 /* /kernel/arch/arm/mach-msm/leds-iyokan.c
  *
- * Copyright (C) [2010] Sony Ericsson Mobile Communications AB.
+ * Copyright 2013 Vassilis Tsogkas (tsogkas@ceid.upatras.gr)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2, as
@@ -8,60 +8,95 @@
  * of the License, or (at your option) any later version.
  */
 
+#ifdef CONFIG_LEDS_AS3676
+#include <linux/kernel.h>
+#include <linux/errno.h>
 #include <linux/leds-as3676.h>
-#include <linux/leds.h>
-#include <linux/bug.h>
-
-static struct as3676_platform_led as3676_leds_mapping[] = {
-	{
-		.name = "lcd-backlight",
-		.sinks = BIT(AS3676_SINK_01),
-		.flags = AS3676_FLAG_PWM_CTRL | AS3676_FLAG_PWM_INIT
-			| AS3676_FLAG_WAIT_RESUME,
-		.max_current = 20000,
-		.hw_max_current = 25000,
-		.default_brightness = LED_FULL,
-	},
-	{
-		.name = "button-backlight",
-		.sinks = BIT(AS3676_SINK_RGB1),
-		.max_current = 4000,
-		.hw_max_current = 25000,
-	},
-	{
-		.name = "keyboard-backlight",
-		.sinks = BIT(AS3676_SINK_32) | BIT(AS3676_SINK_33),
-		.max_current = 38000,
-		.hw_max_current = 38000,
-	},
-	{
-		.name = "red",
-		.sinks = BIT(AS3676_SINK_41),
-		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 2000,
-		.hw_max_current = 25000,
-	},
-	{
-		.name = "green",
-		.sinks = BIT(AS3676_SINK_42),
-		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 2000,
-		.hw_max_current = 25000,
-	},
-	{
-		.name = "blue",
-		.sinks = BIT(AS3676_SINK_43),
-		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 2000,
-		.hw_max_current = 25000,
-	},
-};
 
 struct as3676_platform_data as3676_platform_data = {
-	.leds = as3676_leds_mapping,
-	.num_leds = ARRAY_SIZE(as3676_leds_mapping),
-	.als_connected = 1,
-	.als_wait = 100,
-	.dls_connected = false,
-	.ldo_mV = 3300,
+	.step_up_vtuning = 18,	/* 0 .. 31 uA on DCDC_FB */
+	.audio_speed_down = 1,	/* 0..3 resp. 0, 200, 400, 800ms */
+	.audio_speed_up = 4,	/* 0..7 resp. 0, 50, 100, 150,
+					200,250,400, 800ms */
+	.audio_agc_ctrl = 1,	/* 0 .. 7: 0==no AGC, 7 very aggressive*/
+	.audio_gain = 7,	/* 0..7: -12, -6,  0, 6
+					12, 18, 24, 30 dB */
+	.audio_source = 2,	/* 0..3: 0=curr33, 1=DCDC_FB
+					2=GPIO1,  3=GPIO2 */
+	.step_up_lowcur = true,
+	.reset_on_i2c_shutdown = true,
+	.caps_mounted_on_dcdc_feedback = 1,
+	.cp_control = 0x10,
+	.leds[0] = {
+		.name = "lcd-backlight",
+		.on_charge_pump = 0,
+		.max_current_uA = 20000,
+		.startup_current_uA = 4000,
+		.use_dls = true,
+	},
+	.leds[1] = {
+		.name = "led_2-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
+	.leds[2] = {
+		.name = "led_3-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
+	.leds[3] = {
+		.name = "button-backlight",
+		.on_charge_pump = 0,
+		.max_current_uA = 4000,
+		.startup_current_uA = 4000,
+	},
+	.leds[4] = {
+		.name = "keyboard-backlight-rgb1",
+		.on_charge_pump = 0,
+		.max_current_uA = 38000,
+		.startup_current_uA = 38000,
+	},
+	.leds[5] = {
+		.name = "keyboard-backlight-rgb2",
+		.on_charge_pump = 0,
+		.max_current_uA = 38000,
+		.startup_current_uA = 38000,
+	},
+	.leds[6] = {
+		.name = "red",
+		.on_charge_pump = 1,
+		.max_current_uA = 2000,
+	},
+	.leds[7] = {
+		.name = "green",
+		.on_charge_pump = 1,
+		.max_current_uA = 2000,
+	},
+	.leds[8] = {
+		.name = "blue",
+		.on_charge_pump = 1,
+		.max_current_uA = 3000,
+	},
+	.leds[9] = {
+		.name = "led_10-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
+	.leds[10] = {
+		.name = "led_11-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
+	.leds[11] = {
+		.name = "led_12-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
+	.leds[12] = {
+		.name = "led_13-not-connected",
+		.on_charge_pump = 0,
+		.max_current_uA = 0,
+	},
 };
+
+#endif
