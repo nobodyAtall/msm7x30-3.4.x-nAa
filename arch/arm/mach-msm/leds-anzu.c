dc096c54004d67880dd8a808cf19cf206a0ec40b
Michael Bestas <mikeioannina@gmail.com>
2013-05-21 03:24:06 +0300

    mogami: update as3676 platform data
    Fix build for urushi
    Use dls flag taken from previous implementation, fixes urushi lcd-backlight & probably others
    Small typo fixes
    
    Change-Id: I307294f10e4e259d1bb5573e2f00ca763c6a2b69
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- arch/arm/mach-msm/leds-anzu.c
+++ arch/arm/mach-msm/leds-anzu.c
@@ -32,7 +32,7 @@ struct as3676_platform_data as3676_platform_data = {
 		.on_charge_pump = 0,
 		.max_current_uA = 20000,
 		.startup_current_uA = 4000,
-		.use_dls = true,
+		.use_dls = false,
 	},
 	.leds[1] = {
 		.name = "led_2-not-connected",
