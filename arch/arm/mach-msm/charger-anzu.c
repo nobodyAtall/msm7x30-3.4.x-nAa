236875b78b1d38706adbddc52d5c898b9fa58f98
Michael Bestas <mikeioannina@gmail.com>
2013-06-25 21:39:31 +0300

    charger: update device-specific data
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ieb481c67d1abfe1ae4c8784caadac4994c0ddf11

--- arch/arm/mach-msm/charger-anzu.c
+++ arch/arm/mach-msm/charger-anzu.c
@@ -1,6 +1,7 @@
-/* /kernel/arch/arm/mach-msm/charger-anzu.c
+/* arch/arm/mach-msm/charger-anzu.c
  *
  * Copyright (C) 2010 Sony Ericsson Mobile Communications AB.
+ * Copyright (C) 2013 LegacyXperia Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2, as
@@ -10,12 +11,20 @@
 
 #include <linux/battery_chargalg.h>
 
+static struct battery_regulation_vs_temperature id_bat_reg = {
+	/* Cold, Normal, Warm, Overheat */
+	{5, 45,		55,	127},	/* battery temp */
+	{0, 4200,	4000,	0},	/* charge volt */
+	{0, USHRT_MAX,	400,	0},	/* charge curr */
+};
+
 static struct ambient_temperature_limit limit_tbl = {
 	{0,	42,	43,	127},	/* ambient temp: base */
 	{4,	1,	37,	0},	/* ambient temp: hysteresis */
 };
 
 struct device_data device_data = {
+	.id_bat_reg = &id_bat_reg,
 	.limit_tbl = &limit_tbl,
 	.battery_capacity_mah = 1500,
 	.maximum_charging_current_ma = 1050,
--- arch/arm/mach-msm/charger-iyokan.c
+++ arch/arm/mach-msm/charger-iyokan.c
@@ -1,6 +1,7 @@
-/* /kernel/arch/arm/mach-msm/charger-iyokan.c
+/* arch/arm/mach-msm/charger-iyokan.c
  *
  * Copyright (C) 2010 Sony Ericsson Mobile Communications AB.
+ * Copyright (C) 2013 LegacyXperia Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2, as
@@ -10,7 +11,15 @@
 
 #include <linux/battery_chargalg.h>
 
+static struct battery_regulation_vs_temperature id_bat_reg = {
+	/* Cold, Normal, Warm, Overheat */
+	{5, 45,		55,	127},	/* battery temp */
+	{0, 4200,	4000,	0},	/* charge volt */
+	{0, USHRT_MAX,	400,	0},	/* charge curr */
+};
+
 struct device_data device_data = {
+	.id_bat_reg = &id_bat_reg,
 	.battery_capacity_mah = 1500,
 	.maximum_charging_current_ma = 1050,
 };
