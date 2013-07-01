e67ca25b3a12db094a22a019661db1c09a4737b6
Michael Bestas <mikeioannina@gmail.com>
2013-07-01 20:49:26 +0300

    PMEM hack for compatibility with camera libs
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I2a05277782e5a9e107453f445b0c193077701010

--- drivers/misc/pmem.c
+++ drivers/misc/pmem.c
@@ -1652,14 +1652,16 @@ static int pmem_mmap(struct file *file, struct vm_area_struct *vma)
 	down_write(&data->sem);
 	/* check this file isn't already mmaped, for submaps check this file
 	 * has never been mmaped */
-	if ((data->flags & PMEM_FLAGS_SUBMAP) ||
-	    (data->flags & PMEM_FLAGS_UNSUBMAP)) {
+	if (data->flags & PMEM_FLAGS_MASTERMAP) {
+		if ((data->flags & PMEM_FLAGS_SUBMAP) ||
+		   (data->flags & PMEM_FLAGS_UNSUBMAP)) {
 #if PMEM_DEBUG
-		pr_err("pmem: you can only mmap a pmem file once, "
-		       "this file is already mmaped. %x\n", data->flags);
+			pr_err("pmem: you can only mmap a pmem file once, "
+			       "this file is already mmaped. %x\n", data->flags);
 #endif
-		ret = -EINVAL;
-		goto error;
+			ret = -EINVAL;
+			goto error;
+		}
 	}
 	/* if file->private_data == unalloced, alloc*/
 	if (data->index == -1) {
