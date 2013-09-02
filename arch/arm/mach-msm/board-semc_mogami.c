d7a6d7f07ff78fbfed31b389efde35587e4de53b
Michael Bestas <mikeioannina@gmail.com>
2013-09-02 05:18:38 +0300

    mogami-board: Add PM8XXX misc driver
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I09e0f74ed7422535b7e97a89d793bf537adbd0cf

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -574,12 +574,17 @@ static struct pm8xxx_rtc_platform_data pm8xxx_rtc_pdata = {
 	.rtc_alarm_powerup	= false,
 };
 
+static struct pm8xxx_misc_platform_data pm8xxx_misc_pdata = {
+	.priority		= 0,
+};
+
 static struct pm8058_platform_data pm8058_7x30_data = {
 	.irq_pdata		= &pm8xxx_irq_pdata,
 	.gpio_pdata		= &pm8xxx_gpio_pdata,
 	.mpp_pdata		= &pm8xxx_mpp_pdata,
 	.vibrator_pdata		= &pm8xxx_vibrator_pdata,
 	.rtc_pdata		= &pm8xxx_rtc_pdata,
+	.misc_pdata		= &pm8xxx_misc_pdata,
 };
 
 #ifdef CONFIG_MSM_SSBI
