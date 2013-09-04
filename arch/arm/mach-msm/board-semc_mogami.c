9b4cca11a721b5b21e588ef45fe0829be2cbae3d
Michael Bestas <mikeioannina@gmail.com>
2013-09-05 00:40:00 +0300

    Revert "mogami-board: add PM8XXX RTC clock"
    Causes issues with alarms
    
    This reverts commit 89e4b22c54357648268739f7d3f6fccfafa33502.
    
    Change-Id: Ie97780fa22b51bab30121e22c908c521f9590acf
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -568,11 +568,6 @@ static struct pm8xxx_vibrator_platform_data pm8xxx_vibrator_pdata = {
 	.max_timeout_ms		= 15000,
 };
 
-static struct pm8xxx_rtc_platform_data pm8xxx_rtc_pdata = {
-	.rtc_write_enable	= true,
-	.rtc_alarm_powerup	= false,
-};
-
 static struct pm8xxx_misc_platform_data pm8xxx_misc_pdata = {
 	.priority		= 0,
 };
@@ -582,7 +577,6 @@ static struct pm8058_platform_data pm8058_7x30_data = {
 	.gpio_pdata		= &pm8xxx_gpio_pdata,
 	.mpp_pdata		= &pm8xxx_mpp_pdata,
 	.vibrator_pdata		= &pm8xxx_vibrator_pdata,
-	.rtc_pdata		= &pm8xxx_rtc_pdata,
 	.misc_pdata		= &pm8xxx_misc_pdata,
 };
 
