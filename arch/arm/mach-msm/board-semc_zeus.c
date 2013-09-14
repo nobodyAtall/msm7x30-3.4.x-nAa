39b6ff6d5ea644fc62bcd8ce93a516e2cd22ccc7
Michael Bestas <mikeioannina@gmail.com>
2013-09-14 18:53:32 +0300

    zeus-board: Add PM8XXX misc driver
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I98442380fd860c295d3c8d265546363f75a21bd0

--- arch/arm/mach-msm/board-semc_zeus.c
+++ arch/arm/mach-msm/board-semc_zeus.c
@@ -442,11 +442,16 @@ static struct pm8xxx_vibrator_platform_data pm8xxx_vibrator_pdata = {
 	.max_timeout_ms		= 15000,
 };
 
+static struct pm8xxx_misc_platform_data pm8xxx_misc_pdata = {
+	.priority		= 0,
+};
+
 static struct pm8058_platform_data pm8058_7x30_data = {
 	.irq_pdata		= &pm8xxx_irq_pdata,
 	.gpio_pdata		= &pm8xxx_gpio_pdata,
 	.mpp_pdata		= &pm8xxx_mpp_pdata,
 	.vibrator_pdata		= &pm8xxx_vibrator_pdata,
+	.misc_pdata		= &pm8xxx_misc_pdata,
 };
 
 #ifdef CONFIG_MSM_SSBI
