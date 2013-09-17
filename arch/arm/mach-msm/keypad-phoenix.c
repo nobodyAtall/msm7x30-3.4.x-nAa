221dc0184f41b34ffc527aed866b9e5733f37808
Michael Bestas <mikeioannina@gmail.com>
2013-09-17 05:38:29 +0300

    Initial support for phoenix keypad
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ia3e389d3dc076623f23a0668f3af7a80e53327af

---
+++ arch/arm/mach-msm/keypad-phoenix.c
@@ -0,0 +1,37 @@
+/* arch/arm/mach-msm/keypad-phoenix.c
+ *
+ * Copyright (C) [2010] Sony Ericsson Mobile Communications AB.
+ * Adapted for SEMC 2011 devices by Michael Bestas (mikeioannina@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#include <linux/mfd/pmic8058.h>
+#include "keypad-semc.h"
+
+static const unsigned int pm8xxx_keymap[] = {
+	KEY(7, 0, KEY_VOLUMEUP),
+	KEY(7, 1, KEY_VOLUMEDOWN),
+};
+
+static struct matrix_keymap_data pm8xxx_keymap_data = {
+	.keymap_size	= ARRAY_SIZE(pm8xxx_keymap),
+	.keymap		= pm8xxx_keymap,
+};
+
+struct pm8xxx_keypad_platform_data pm8xxx_keypad_data = {
+	.input_name		= "pm8xxx-keypad",
+	.input_phys_device	= "pm8xxx-keypad/input0",
+	.num_rows		= 8,
+	.num_cols		= 2,
+	.rows_gpio_start	= PM8058_GPIO_PM_TO_SYS(8),
+	.cols_gpio_start	= PM8058_GPIO_PM_TO_SYS(0),
+	.debounce_ms		= 10,
+	.scan_delay_ms		= 32,
+	.row_hold_ns		= 91500,
+	.wakeup			= 1,
+	.keymap_data		= &pm8xxx_keymap_data,
+};
